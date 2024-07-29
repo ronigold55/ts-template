b698ba19ae0480490eaf9d5b5ce169a66c15c105
Ori <ori@example.com>
2024-07-29 21:25:05 +0300

    ex solve

--- src/controllers/productControllers.ts
+++ src/controllers/productControllers.ts
@@ -1,14 +1,30 @@
 import express, {Request, Response, NextFunction} from "express";
-import { getAllProducts } from "../services/productService";
+import { getProducts } from "../services/productService";
 
 export const prodcutRouter = express.Router();
 
 prodcutRouter.get("/products", async (req: Request, res: Response, next: NextFunction)=>{
     try {
-        const products = await getAllProducts();
+        const products = await getProducts();
         res.status(200).json(products);
     } catch (error) {
         console.log(error);        
         res.status(500).send("Error. please try again later")
     }
 })
+prodcutRouter.get("/products/:id", async (req: Request, res: Response, next: NextFunction)=>{
+    try {
+        const product = await getProducts(+req.params.id);
+        res.status(200).json(product[0]);
+    } catch (error) {
+        
+        if (error.message.includes("product id not found")){
+            res.status(404).send("ID not found")
+            return
+        }
+
+        console.log(error);        
+        res.status(500).send("Error. please try again later")
+    }
+})
+
--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,10 +1,18 @@
 import runQuery from "../db/dal";
 import ProdcutModel from "../models/productModel";
 
-export async function getAllProducts(): Promise<ProdcutModel[]> {
-    const q = `SELECT * FROM product;`
+export async function getProducts(id: number=undefined): Promise<ProdcutModel[]> {
+    let q = `SELECT * FROM product`
+
+    if (id)
+        q += ` WHERE id = ${id}`
+
     const res = await runQuery(q);
-    
-    const products = res.map((p)=>new ProdcutModel(p));        
+
+    if (res.length === 0){
+        throw Error("product id not found")
+    }
+
+    const products = res.map((p) => new ProdcutModel(p));
     return products
 }
