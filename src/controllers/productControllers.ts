6b9f71a0727bc9138a0c2d38bed767c92ac40159
Ori <ori@example.com>
2024-08-01 18:49:27 +0300

    class_24

--- src/controllers/productControllers.ts
+++ src/controllers/productControllers.ts
@@ -1,30 +1,43 @@
-import express, {Request, Response, NextFunction} from "express";
-import { getProducts } from "../services/productService";
+import express, { Request, Response, NextFunction } from "express";
+import { addProdcut, getProducts } from "../services/productService";
+import ProdcutModel from "../models/productModel";
 
 export const prodcutRouter = express.Router();
 
-prodcutRouter.get("/products", async (req: Request, res: Response, next: NextFunction)=>{
+prodcutRouter.get("/products", async (req: Request, res: Response, next: NextFunction) => {
     try {
         const products = await getProducts();
         res.status(200).json(products);
     } catch (error) {
-        console.log(error);        
+        console.log(error);
         res.status(500).send("Error. please try again later")
     }
 })
-prodcutRouter.get("/products/:id", async (req: Request, res: Response, next: NextFunction)=>{
+
+prodcutRouter.get("/products/:id", async (req: Request, res: Response, next: NextFunction) => {
     try {
         const product = await getProducts(+req.params.id);
         res.status(200).json(product[0]);
     } catch (error) {
-        
-        if (error.message.includes("product id not found")){
+
+        if (error.message.includes("product id not found")) {
             res.status(404).send("ID not found")
             return
         }
 
-        console.log(error);        
+        console.log(error);
         res.status(500).send("Error. please try again later")
     }
 })
 
+prodcutRouter.post("/products", async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+        const newProduct = new ProdcutModel(req.body);
+        await addProdcut(newProduct);
+        res.status(201).send("ok");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Errro");
+    }
+})
--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,18 +1,25 @@
 import runQuery from "../db/dal";
 import ProdcutModel from "../models/productModel";
 
-export async function getProducts(id: number=undefined): Promise<ProdcutModel[]> {
-    let q = `SELECT * FROM product`
+export async function getProducts(id?: number): Promise<ProdcutModel[]> {
+    let q = `SELECT * FROM product`;
 
     if (id)
         q += ` WHERE id = ${id}`
 
     const res = await runQuery(q);
 
-    if (res.length === 0){
-        throw Error("product id not found")
+    if (res.length === 0 && id){
+        throw new Error("product id not found")
     }
 
     const products = res.map((p) => new ProdcutModel(p));
     return products
+}
+
+export async function addProdcut(p:ProdcutModel) {
+    const q = `INSERT INTO product (name, description, price) 
+                VALUES ('${p.name}', '${p.description || ""}', ${p.price})`;
+        
+    await runQuery(q);
 }
